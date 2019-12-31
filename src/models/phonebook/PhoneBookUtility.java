596dc55b4e0e63e38adb8de5443df18eff38b3fe
Bíró Botond <biro.botond20@gmail.com>
2019-12-31 19:06:25 +0200

    refactor(PhoneBookUtility): make all methods static

--- src/models/phonebook/PhoneBookUtility.java
+++ src/models/phonebook/PhoneBookUtility.java
@@ -9,54 +9,46 @@ import java.util.stream.Collectors;
 
 public class PhoneBookUtility {
 
-  public List<Contact> filterByTitle(List<Contact> contacts, Title title) {
+  public static List<Contact> filterByTitle(List<Contact> contacts, Title title) {
     return contacts.stream()
         .filter(contact -> contact.getTitle().equals(title))
         .collect(Collectors.toList());
   }
 
-  public List<Contact> filterByFirstName(List<Contact> contacts, String firstName) {
+  public static List<Contact> filterByFirstName(List<Contact> contacts, String firstName) {
     return contacts.stream()
         .filter(contact -> contact.getFirstName().equals(firstName))
         .collect(Collectors.toList());
   }
 
-  public List<Contact> filterByLastName(List<Contact> contacts, String lastName) {
+  public static List<Contact> filterByLastName(List<Contact> contacts, String lastName) {
     return contacts.stream()
         .filter(contact -> contact.getLastName().equals(lastName))
         .collect(Collectors.toList());
   }
 
-  public List<Contact> filterByName(List<Contact> contacts, String name) {
+  public static List<Contact> filterByName(List<Contact> contacts, String name) {
     return contacts.stream()
         .filter(contact -> (contact.getFirstName().equals(name) || contact.getLastName().equals(name)))
         .collect(Collectors.toList());
   }
 
-  public List<Contact> filterByPhoneNumber(List<Contact> contacts, String phoneNumber) {
+  public static List<Contact> filterByPhoneNumber(List<Contact> contacts, String phoneNumber) {
     return contacts.stream()
         .filter(contact -> contact.getPhoneNumbers().contains(phoneNumber))
         .collect(Collectors.toList());
   }
 
-  public List<Contact> filterByDate(List<Contact> contacts, String fromDate, String toDate) {
+  public static List<Contact> filterByDate(List<Contact> contacts, String fromDate, String toDate) {
     return contacts.stream()
         .filter(contact -> contact.isBirthdayWithinRange(fromDate, toDate))
         .collect(Collectors.toList());
   }
 
-  public List<Contact> filterByAddress(List<Contact> contacts, String query) {
+  public static List<Contact> filterByAddress(List<Contact> contacts, String query) {
 
     return contacts.stream()
-        .filter(contact -> {
-          List<Address> contactAddresses = contact.getAddresses();
-          for (Address address : contactAddresses) {
-            if (address.hasFieldMatch(query)) {
-              return true;
-            }
-          }
-          return false;
-        })
+        .filter(contact -> contact.hasMatchingAddressField(query))
         .collect(Collectors.toList());
   }
 }
